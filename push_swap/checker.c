888ce4215056565b74c78063c3c8c1b7ef18d8a3
larstenhagen <63248028+larstenhagen@users.noreply.github.com>
2022-04-14 19:51:17 +0200

    Add files via upload

--- push_swap/checker.c
+++ push_swap/checker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltenhage <ltenhage@student.42.fr>          +#+  +:+       +#+        */
+/*   By: larstenhagen <larstenhagen@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 13:29:35 by ltenhage          #+#    #+#             */
-/*   Updated: 2022/04/13 19:29:46 by ltenhage         ###   ########.fr       */
+/*   Updated: 2022/04/14 19:47:29 by larstenhage      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,11 +52,28 @@ void	print_checker_res(t_numbers **stack_a, t_numbers **stack_b, int size)
 		free_stack(stack_b);
 }
 
+static int	ft_print_result(t_numbers **stack_a, t_numbers **stack_b, int size)
+{
+	char	*line;
+
+	line = get_next_line(0);
+	while (line)
+	{
+		if (do_commands(line, stack_a, stack_b, size))
+		{
+			ft_error("Error");
+			return (-1);
+		}
+		free(line);
+		line = get_next_line(0);
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_numbers	**stack_a;
 	t_numbers	**stack_b;
-	char		*line;
 
 	if (argc < 2 || ft_check_args(argc, argv) == 0)
 	{
@@ -68,16 +85,7 @@ int	main(int argc, char **argv)
 	*stack_a = NULL;
 	*stack_b = NULL;
 	init_stack(stack_a, argc, argv);
-	line = get_next_line(0);
-	while (line)
-	{
-		if (do_commands(line, stack_a, stack_b, argc - 1))
-		{
-			ft_error("Error");
-			return (0);
-		}
-		free(line);
-		line = get_next_line(0);
-	}
+	if (ft_print_result(stack_a, stack_b, argc - 1) == -1)
+		return (-1);
 	print_checker_res(stack_a, stack_b, argc - 1);
 }
